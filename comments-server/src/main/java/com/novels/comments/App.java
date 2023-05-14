8cae3337cf93a0d6da780eb608e3ec5b1563c2a5
weishengwx <2234346829@qq.com>
2023-05-14 18:06:51 +0800

    build: 初始化推荐小说项目

---
+++ comments-server/src/main/java/com/novels/comments/App.java
@@ -0,0 +1,13 @@
+package com.novels.comments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
