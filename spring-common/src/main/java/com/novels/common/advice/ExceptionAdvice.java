ed50427acd985c598697e62056f5f73ca875f918
weishengwx <2234346829@qq.com>
2023-07-24 23:05:29 +0800

    feat: spring-common ExceptionAdvice 异常处理信息变更

--- spring-common/src/main/java/com/novels/common/advice/ExceptionAdvice.java
+++ spring-common/src/main/java/com/novels/common/advice/ExceptionAdvice.java
@@ -1,10 +1,11 @@
-package com.novels.user.advic;
+package com.novels.common.advice;
 
 import com.novels.common.bean.Result;
 import com.novels.common.enums.SystemConstantEnum;
 import com.wx.common.exception.BaseException;
 import com.wx.common.exception.BizException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,29 +18,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionAdvice {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public Result<Void> messageNotReadable(HttpMessageNotReadableException messageNotReadableException) {
-        log.info("请检查:异常{}",messageNotReadableException.getMessage());
+        log.error("server-name:{} fail message: {}",applicationName,messageNotReadableException.getMessage());
         return Result.fail();
     }
 
 
     @ExceptionHandler(BizException.class)
     public Result<Void> bizException(BizException bizException) {
-        // BaseException
-        log.error("{}",bizException.getMessage(),bizException);
+        log.error("server-name:{} fail message: {}",applicationName,bizException.getMessage(),bizException);
         return Result.fail(bizException);
     }
 
     @ExceptionHandler(BaseException.class)
     public Result<Void> baseException(BaseException baseException) {
-        // BaseException
-        log.info("{}",baseException.getMessage());
+        log.error("server-name:{} fail message: {}",applicationName,baseException.getMessage());
         return Result.fail(baseException);
     }
 
     @ExceptionHandler({Exception.class,Throwable.class})
     public Result<Void> exception(Throwable throwable) {
+        log.error("server-name:{} Exception :{} ,fail message: {}",applicationName,throwable.getClass(),throwable.getMessage());
         return Result.fail(new BizException(SystemConstantEnum.FAIL,throwable));
     }
 
